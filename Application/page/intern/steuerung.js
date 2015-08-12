9d6a46abe31d64b50622ffd4710765e32dc7e632
Mel-Tem <MeltemOezkul@gmail.com>
2015-08-12 13:00:53 +0200

    steuerung änderung

--- Application/page/intern/steuerung.js
+++ Application/page/intern/steuerung.js
@@ -3,32 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+	/// Code für die Steuerung!!!		
+				var updateFcts	= [];
 
-	var renderer	= new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
-
-	var updateFcts	= [];
-	var scene	= new THREE.Scene();
-	var camera	= new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000 );
-	camera.position.z = 5;
-        camera.position.y = 3;
-        camera.rotation.x = -0.785;
 
         //hinzufügen der Objekte
 	
-	var geometry	= new THREE.CubeGeometry( 1, 1, 1);
-	var material	= new THREE.MeshBasicMaterial({color: 0x00ff00});
-	var mesh	= new THREE.Mesh( geometry, material );	
-        scene.add( mesh );
-         
-        mesh.position.y=10;
-
-       
-        var light = new THREE.AmbientLight( 0x404040 ); // soft white light
-        scene.add( light );
+	var geometry	= new THREE.CubeGeometry( 3, 3, 3);
+	var material	= new THREE.MeshBasicMaterial({color: "rgb(250, 235, 27)"});
+	var snake	= new THREE.Mesh( geometry, material );
 	
-	// tastaturbewegung
+        scene.add( snake );
+		
+		snake.position.y = 3;
+
+		// tastaturbewegung
 	
 	var keyboard	= new THREEx.KeyboardState(renderer.domElement);
 	renderer.domElement.setAttribute("tabIndex", "0");
@@ -36,14 +25,14 @@
 	
 	updateFcts.push(function(){
 		if( keyboard.pressed('left') ){
-			mesh.position.x -= 0.01;			
+			snake.position.x -= 0.1;			
 		}else if( keyboard.pressed('right') ){
-			mesh.position.x += 0.01;
+			snake.position.x += 0.1;
 		}
 		if( keyboard.pressed('down') ){
-			mesh.position.z += 0.01;		
+			snake.position.z += 0.1;		
 		}else if( keyboard.pressed('up') ){
-			mesh.position.z -= 0.01;		
+			snake.position.z -= 0.1;		
 		}
 	})
 
@@ -67,4 +56,6 @@
             updateFn(deltaMsec/1000, nowMsec/1000)
             })
 	})
-        
+	
+	
+	///Ende vom Code der Steuerung!!!

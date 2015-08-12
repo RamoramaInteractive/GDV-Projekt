9a3d7272eb50c1335dd88813eb219dd80e1e2105
Kathilein <k.kahlert@googlemail.com>
2015-08-12 15:06:43 +0200

    Kollision verbinden

---
+++ 1. Meilenstein/Kollision/verbindung/steuerung.js
@@ -0,0 +1,60 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+	/// Code für die Steuerung!!!		
+				var updateFcts	= [];
+
+    //hinzufügen der Objekte
+	
+	var geometry	= new THREE.CubeGeometry( 3, 3, 3);
+	var material	= new THREE.MeshBasicMaterial({color: "rgb(250, 235, 27)"});
+	var snake	= new THREE.Mesh( geometry, material );
+	
+        scene.add( snake );
+		
+		snake.position.y = 10;
+
+	// tastaturbewegung
+	
+	var keyboard	= new THREEx.KeyboardState(renderer.domElement);
+	renderer.domElement.setAttribute("tabIndex", "0");
+	renderer.domElement.focus();
+	
+	updateFcts.push(function(){
+		if( keyboard.pressed('left') ){
+			snake.position.x -= 0.1;			
+		}else if( keyboard.pressed('right') ){
+			snake.position.x += 0.1;
+		}
+		if( keyboard.pressed('down') ){
+			snake.position.z += 0.1;		
+		}else if( keyboard.pressed('up') ){
+			snake.position.z -= 0.1;		
+		}
+	})
+
+	//render
+	updateFcts.push(function(){
+		renderer.render( scene, camera );
+                collision();
+	})
+
+	//loop runner
+	var lastTimeMsec= null
+	requestAnimationFrame(function animate(nowMsec){
+    
+    // keep looping
+    requestAnimationFrame( animate );
+    // measure time
+         lastTimeMsec	= lastTimeMsec || nowMsec-1000/60
+         var deltaMsec	= Math.min(200, nowMsec - lastTimeMsec)
+         lastTimeMsec	= nowMsec
+	
+    // call each update function
+         updateFcts.forEach(function(updateFn){
+         updateFn(deltaMsec/1000, nowMsec/1000)
+         })
+	})	
+	///Ende vom Code der Steuerung!!!
---
+++ Application/page/intern/kollision.js
@@ -0,0 +1,70 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var collisionList = []; //Wand 
+
+var wandaufbau_wo = new THREE.CubeGeometry(2, 10, 40);
+var wandaufbau_sn = new THREE.CubeGeometry(2, 10, 80);
+var wandMaterial = new THREE.MeshBasicMaterial({color: 0x993366	}); //unsichtbar: 0x000000
+var wand_w = new THREE.Mesh(wandaufbau_wo, wandMaterial);
+var wand_o = new THREE.Mesh(wandaufbau_wo, wandMaterial);
+var wand_n = new THREE.Mesh(wandaufbau_sn, wandMaterial);
+var wand_s = new THREE.Mesh(wandaufbau_sn, wandMaterial);
+
+
+collisionList.push(wand_w);
+collisionList.push(wand_o);
+collisionList.push(wand_n);
+collisionList.push(wand_s);
+//
+//scene.add(wand_w);
+//            scene.add(wand_o);
+//            scene.add(wand_n);
+//            scene.add(wand_s);
+            
+             wand_w.position.x = -34.5;
+            wand_o.position.x = 34.5;
+
+            wand_n.rotation.y = 1.57;
+            wand_n.position.z = -18;
+            wand_s.rotation.y = 1.57;
+            wand_s.position.z = 18;
+
+            scene.add(wand_w);
+            scene.add(wand_o);
+            scene.add(wand_n);
+            scene.add(wand_s);
+
+
+function collision()
+{
+
+    var originPoint = snake.position.clone();
+
+
+
+    for (var vertexIndex = 0; vertexIndex < snake.geometry.vertices.length; vertexIndex++)
+    {
+        var localVertex = snake.geometry.vertices[vertexIndex].clone();
+        var globalVertex = localVertex.applyMatrix4(snake.matrix);
+        var directionVector = globalVertex.sub(snake.position);
+
+        var ray = new THREE.Raycaster(originPoint, directionVector.clone().normalize());
+        var collisionResults = ray.intersectObjects(collisionList);
+        if (collisionResults.length > 0 && collisionResults[0].distance < directionVector.length())
+        {
+
+            console.log("Es wurde getroffen!"); //DummyVersion 
+        }
+
+
+    }
+
+
+
+}
+

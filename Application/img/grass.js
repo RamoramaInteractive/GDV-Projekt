4f166d814176bfde59393d4df123370d607dba6c
Kathilein <k.kahlert@googlemail.com>
2015-10-01 23:54:43 +0200

    Umgebung
    Grobe Gestaltung der Umgebung

---
+++ Application/img/grass.js
@@ -0,0 +1,50 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "grass_low_poly.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 11,
+        "faces"         : 4,
+        "normals"       : 0,
+        "colors"        : 0,
+        "uvs"           : 12,
+        "materials"     : 1
+    },
+
+    "scale" : 1.000000,
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "grass_mat",
+	"colorAmbient" : [0.294, 0.267, 0.149],
+	"colorDiffuse" : [0.305882, 0.403922, 0.309804],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAlpha" : "low_poly_grass_opacity.png",
+	"mapAmbient" : "low_poly_grass2.png",
+	"mapBump" : "low_poly_grass_normal.png",
+	"mapDiffuse" : "low_poly_grass2.png",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0,
+	"transparent" : true
+	}],
+
+    "vertices": [-3.415052,-0.142539,4.625601,3.415051,-0.142540,-4.625601,-3.415051,8.379845,4.625600,3.415054,8.379844,-4.625599,-5.749675,-0.142538,0.000000,5.749675,-0.142540,-0.000001,-5.749675,8.379845,0.000001,5.749677,8.379843,0.000001,-0.000000,-0.142539,0.000000,0.000002,11.356812,0.000001,-0.000000,-0.142539,0.000000],
+
+    "morphTargets": [],
+
+    "morphColors": [],
+
+    "normals": [],
+
+    "colors": [],
+
+    "uvs": [[0.002981,0.00307,0.99566,0.00307,0.002982,0.99256,0.99566,0.99256,0.00403,0.004071,0.99601,0.004071,0.00403,0.99356,0.99601,0.99356,0.49932,0.99256,0.49932,0.00307,0.50002,0.99356,0.50002,0.004071]],
+
+    "faces": [11,0,8,9,2,0,0,9,8,2,11,4,10,9,6,0,4,11,10,6,11,1,3,9,8,0,1,3,8,9,11,5,7,9,10,0,5,7,10,11]
+
+}
